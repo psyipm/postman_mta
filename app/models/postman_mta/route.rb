b5bb03f2ce3de3a7df5d7eac9586be59dc262502
Igor Malinovskiy <igor.malinovskiy@netfixllc.org>
2017-09-30 15:06:27 +0300

    Add create and update methods to route

--- app/models/postman_mta/route.rb
+++ app/models/postman_mta/route.rb
@@ -3,5 +3,13 @@ module PostmanMta
     def index
       get('/api/v1/routes')
     end
+
+    def update(uuid, params = {})
+      patch("/api/v1/routes/#{uuid}", body: params)
+    end
+
+    def create(params = {})
+      post('/api/v1/routes', body: params)
+    end
   end
 end
